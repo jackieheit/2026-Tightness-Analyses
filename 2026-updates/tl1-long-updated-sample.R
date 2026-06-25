af44c773a0ef5172ecc54cf7e3a623e92ec41ea8
Jackie <heitmannjackie@gmail.com>
2026-06-25 11:50:02 -0400

    update repo for remote access

--- 2026-updates/tl1-long-updated-sample.R
+++ 2026-updates/tl1-long-updated-sample.R
@@ -25,40 +25,40 @@ test <- tl %>%
 tl1_cols <- c("General_TL1_avg_final","Soc_TL1_avg_final",
                 "Gender_TL1_avg_final","Mar_TL1_avg_final",
                 "Sex_TL1_avg_final", "FM_TL1_avg_final")
-tl2_cols <- c("General_TL")
+tl2_cols <- c("General_TL2_avg_final","Soc_TL2_avg_final",
+              "Gender_TL2_avg_final","Mar_TL2_avg_final",
+              "Sex_TL2_avg_final", "FM_TL2_avg_final")
+tl3_cols <- c("General_TL3_avg_final","Soc_TL3_avg_final",
+              "Gender_TL3_avg_final","Mar_TL3_avg_final",
+              "Sex_TL3_avg_final", "FM_TL3_avg_final")
+tl4_cols <- c("General_TL4_avg_final","Soc_TL4_avg_final",
+              "Gender_TL4_avg_final","Mar_TL4_avg_final",
+              "Sex_TL4_avg_final", "FM_TL4_avg_final")
+
+
+
+tl_cols <- c(tl1_cols, tl2_cols, tl3_cols, tl4_cols)
 
 domain_lookup <- tibble(
-  Domain_var = c(tl1_cols),
-  Domain_name = c(
-    "law",
-    "social",
-    "gender",
-    "marriage",
-    "sex",
-    "funeral"
-  ),
-  Domain_num = 1:8
+  Domain_prefix = c("General", "Soc", "Gender", "Mar", "Sex", "FM"),
+  Domain_name = c("law", "social", "gender", "marriage", "sex", "funeral"),
+  Domain_num = 1:6
 )
 
-
-tl1 <- tl %>%
-  select(OWC, SCCS, all_of(tl1_cols), General_TL5B_avg_final)
-
-tl1_id <- full_join(id, tl1, by = "OWC") %>%
-  select(-SCCS) %>%
-  mutate(
-    Ov_tl_avg = rowMeans(across(all_of(tl1_cols)), na.rm = TRUE),
-    Ov_tl_avg = if_else(is.nan(Ov_tl_avg), NA_real_, Ov_tl_avg)
-  )
-
-tl1_long <- tl1_id %>%
-  pivot_longer(
-    cols = all_of(domain_lookup$Domain_var),
-    values_to = "TL1",
-    names_to = "Domain_var"
+tl_long <- tl %>%
+  select(
+    OWC, SCCS,
+    all_of(tl_cols),
+    General_TL5B_avg_final
   ) %>%
-  left_join(domain_lookup, by = "Domain_var")
+  full_join(id, ., by = "OWC") %>%
+  select(-SCCS) %>%
+  pivot_longer(
+    cols = all_of(tl_cols),
+    names_to = c("Domain_prefix", ".value"),
+    names_pattern = "^(General|Soc|Gender|Mar|Sex|FM)_(TL[1-4])_avg_final$"
+  ) %>%
+  left_join(domain_lookup, by = "Domain_prefix")
 
-
-write.csv(tl1_long, "data/tl1_long.csv", row.names = FALSE)
+write.csv(tl_long, "data/tl_long.csv", row.names = FALSE)
 
